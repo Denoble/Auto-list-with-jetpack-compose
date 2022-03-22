4512218c43d9b5feb5fae6cd330c318eeb325cf7
uchegodfrey <ujay2002@yahoo.com>
2022-03-22 14:42:03 -0400

    lazyColumn fixed

--- app/src/main/java/com/gevcorst/carfaxproject/ui/composables/HomeScreen.kt
+++ app/src/main/java/com/gevcorst/carfaxproject/ui/composables/HomeScreen.kt
@@ -1,47 +1,37 @@
 package com.gevcorst.carfaxproject.ui
 
-import android.widget.Toast
+import androidx.compose.animation.graphics.ExperimentalAnimationGraphicsApi
+import androidx.compose.animation.graphics.res.animatedVectorResource
+import androidx.compose.animation.graphics.res.rememberAnimatedVectorPainter
+import androidx.compose.animation.graphics.vector.AnimatedImageVector
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.magnifier
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.imageResource
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 import androidx.lifecycle.viewmodel.compose.viewModel
-import coil.compose.AsyncImage
-import coil.compose.ImagePainter
 import coil.compose.rememberAsyncImagePainter
-import coil.compose.rememberImagePainter
 import coil.request.ImageRequest
 import com.gevcorst.carfaxproject.R
 import com.gevcorst.carfaxproject.model.Listings
 import com.gevcorst.carfaxproject.viewmodel.CarListViewModel
 import com.gevcorst.carfaxproject.viewmodel.ServiceStatus
-import com.skydoves.landscapist.glide.GlideImage
 
 @Composable
 fun Home(viewModel: CarListViewModel) {
@@ -63,98 +53,107 @@ fun PreviewAppBar() {
 
 @Composable
 fun ProfileCard(listing: Listings) {
-    Surface() {
-        ConstraintLayout(modifier = Modifier
-            .fillMaxWidth()
-            .wrapContentHeight()) {
-            val (image, contents) = createRefs()
-            ProfilePicture(carListing = listing, modifier = Modifier.constrainAs(image) {
-                top.linkTo(parent.top, margin = 16.dp)
-                start.linkTo(parent.start, margin = 32.dp)
-                end.linkTo(parent.end)
-
-            })
-            /*ProfileContent(listing = listing, modifier = Modifier.constrainAs(contents) {
-                end.linkTo(parent.end, margin = 16.dp)
-                top.linkTo(parent.top, margin = 16.dp)
-
-                //start.linkTo(image.end)
-                bottom.linkTo(parent.bottom, margin = 16.dp)
-
-            })*/
-        }
+    Row(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        ProfileContent(listing = listing, modifier = Modifier.wrapContentSize())
     }
 }
 
 @Composable
 fun ProfilePicture(carListing: Listings, modifier: Modifier) {
     Row(modifier = Modifier.size(130.dp)) {
-        val url = remember{carListing.images.firstPhoto.medium}
-        val painter =
-            rememberAsyncImagePainter(ImageRequest.Builder //Used while loading
-                (LocalContext.current).data(data =url).apply(block = fun ImageRequest.Builder.() {
-                crossfade(true) //Crossfade animation between images
-                placeholder(R.drawable.loading_animation) //Used while loading
-                fallback(R.drawable.ic_baseline_broken_image_24) //Used if data is null
-                error(R.drawable.ic_baseline_broken_image_24) //Used when loading returns with error
-            }).build()
-            )
 
-
-        Image(
-            modifier = Modifier
-                .fillMaxSize()
-                .clip(RoundedCornerShape(16.dp)),
-            //Use painter in Image composable
-            painter = painter,
-            contentDescription = stringResource(id = R.string.imageloader)
-        )
     }
 }
 
 @Composable
 fun ProfileContent(listing: Listings, modifier: Modifier) {
-    ConstraintLayout(modifier = Modifier.wrapContentSize()) {
-        val (year, make, model, trim, price, mileage, location) = createRefs()
+    ConstraintLayout(modifier = Modifier.fillMaxSize()) {
+        val (image, year, make, model, trim, price, mileage, location) = createRefs()
+        val url = remember { listing.images.firstPhoto.medium }
+        val painter =
+            rememberAsyncImagePainter(
+                ImageRequest.Builder //Used while loading
+                    (LocalContext.current).data(data = url)
+                    .apply(block = fun ImageRequest.Builder.() {
+                        crossfade(true) //Crossfade animation between images
+                        placeholder(R.drawable.loading_animation) //Used while loading
+                        fallback(R.drawable.ic_baseline_broken_image_24) //Used if data is null
+                        error(R.drawable.ic_baseline_broken_image_24) //Used when loading returns with error
+                    }).build()
+            )
+
+
+        Image(
+            modifier = Modifier
+                .size(80.dp)
+                .clip(RoundedCornerShape(16.dp))
+                .constrainAs(image) {
+                    top.linkTo(parent.top, margin = 16.dp)
+                    start.linkTo(parent.start)
+                    width = Dimension.fillToConstraints
+                    height = Dimension.wrapContent
+                },
+            //Use painter in Image composable
+            painter = painter,
+            contentDescription = stringResource(id = R.string.imageloader)
+        )
         Text(text = listing.year.toString(), modifier = Modifier.constrainAs(year) {
-            top.linkTo(parent.top, margin = 16.dp)
-            start.linkTo(parent.start, margin = 16.dp)
-            end.linkTo(make.start, margin = 16.dp)
+            top.linkTo(image.top, margin = 8.dp)
+            start.linkTo(image.end, margin = 16.dp)
+            width = Dimension.fillToConstraints
+            height = Dimension.wrapContent
         }, textAlign = TextAlign.Start)
-        Text(text = listing.make.toString(), modifier = Modifier.constrainAs(make) {
+        Text(text = listing.make, modifier = Modifier.constrainAs(make) {
             top.linkTo(year.top, margin = 16.dp)
-            end.linkTo(model.start, margin = 16.dp)
-        })
-        Text(text = listing.model.toString(), modifier = Modifier.constrainAs(model) {
+            start.linkTo(year.end, margin = 16.dp)
+            baseline.linkTo(year.baseline)
+            width = Dimension.fillToConstraints
+            height = Dimension.wrapContent
+        }, textAlign = TextAlign.Start)
+        Text(text = listing.model, modifier = Modifier.constrainAs(model) {
             top.linkTo(make.top, margin = 16.dp)
-            end.linkTo(trim.start, margin = 16.dp)
+            start.linkTo(make.end, margin = 16.dp)
+            baseline.linkTo(make.baseline)
+            width = Dimension.fillToConstraints
+            height = Dimension.wrapContent
         })
-        Text(text = listing.trim.toString(), modifier = Modifier.constrainAs(trim) {
-            top.linkTo(make.top, margin = 16.dp)
+        Text(text = listing.trim, modifier = Modifier.constrainAs(trim) {
+            top.linkTo(model.top, margin = 16.dp)
+            start.linkTo(model.end, margin = 16.dp)
             end.linkTo(parent.end, margin = 16.dp)
+            baseline.linkTo(model.baseline)
+            width = Dimension.fillToConstraints
+            height = Dimension.wrapContent
         })
         Text(
             text = stringResource(id = R.string.dollar_sign) + listing.currentPrice.toString(),
             modifier = Modifier.constrainAs(price) {
                 top.linkTo(year.bottom, margin = 16.dp)
-                start.linkTo(year.start, margin = 16.dp)
-                end.linkTo(mileage.start)
+                start.linkTo(year.start)
+                width = Dimension.fillToConstraints
+                height = Dimension.wrapContent
             })
         Text(
             text = listing.currentPrice.toString() + stringResource(id = R.string.mileage_symbol),
             modifier = Modifier.constrainAs(mileage) {
                 top.linkTo(price.top, margin = 16.dp)
-                end.linkTo(trim.end, margin = 16.dp)
-            },
-            textAlign = TextAlign.Start
+                start.linkTo(price.end, margin = 16.dp)
+                baseline.linkTo(price.baseline)
+                width = Dimension.fillToConstraints
+                height = Dimension.wrapContent
+            }
         )
         Text(
             text = listing.dealer.city + " " + listing.dealer.state,
             modifier = Modifier.constrainAs(location) {
                 top.linkTo(price.bottom, margin = 16.dp)
                 start.linkTo(price.start)
-                end.linkTo(mileage.end, margin = 16.dp)
-            }, textAlign = TextAlign.Start
+                width = Dimension.fillToConstraints
+                height = Dimension.wrapContent
+            }
         )
     }
 
@@ -183,169 +182,87 @@ private fun AppBar(viewModel: CarListViewModel) {
                 backgroundColor = MaterialTheme.colors.primary
             )
         },
-        drawerContent = { Text(text = "Drawer Menu 1") },
         content = {
-            ConstraintLayout(modifier = Modifier.fillMaxSize()) {
-                val(lazyColum) = createRefs()
-                LazyColumn(
-                    modifier = Modifier
-                        .constrainAs(lazyColum){
-                            top.linkTo(parent.top, margin = 16.dp)
-                            start.linkTo(parent.start, margin = 16.dp)
-                            end.linkTo(parent.end, margin = 16.dp)
-                            bottom.linkTo(parent.bottom, margin = 16.dp)
-                        }
-                        .padding(16.dp)
-                        .background(Color.White)
-                        .fillMaxSize()
-                ) {
-                    when (serviceStatus.value) {
-                        ServiceStatus.IDLE, ServiceStatus.LOADING -> {
-                            items(listings.value) { listing ->
-                                CustomCircularProgressBar()
-                            }
-                        }
-                        ServiceStatus.DONE -> {
-                            items(listings.value) { listing ->
-                                ProfileCard(listing = listing)
-                            }
-                        }
-                        else -> {
-
-                        }
-
-                    }
-
+            when (serviceStatus.value) {
+                ServiceStatus.IDLE, ServiceStatus.LOADING -> {
+                    LoadingAnimations()
                 }
+                ServiceStatus.DONE -> {
+                    LoadItemList(listings = listings)
+                }
+                else -> {
+                    LoadingAnimations()
+                }
+
             }
         },
-        bottomBar = { BottomAppBar(backgroundColor = MaterialTheme.colors.primary) { Text("Bottom App Bar") } }
+        bottomBar = {
+            BottomAppBar(backgroundColor = MaterialTheme.colors.primary) {
+                Text(
+                    stringResource(id = R.string.app_name)
+                )
+            }
+        }
     )
 }
 
 @Composable
-fun ListRow(carList: Listings) {
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier
-            .wrapContentHeight()
-            .fillMaxWidth()
-            .background(Color.White)
-    ) {
-        AsyncImage(
-            model = ImageRequest.Builder(LocalContext.current)
-                .data(carList.images.firstPhoto.large)
-                .crossfade(true)
-                .build(),
-            placeholder = painterResource(R.drawable.loading_animation),
-            contentDescription = stringResource(R.string.imageloader),
-            contentScale = ContentScale.Inside,
-            error = painterResource(R.drawable.ic_baseline_broken_image_24),
+fun LoadItemList(listings: State<List<Listings>>) {
+    ConstraintLayout(modifier = Modifier.fillMaxSize()) {
+        val (lazyColum) = createRefs()
+        LazyColumn(
             modifier = Modifier
-                .clip(CircleShape)
-                .size(100.dp)
-        )
-
-        Column() {
-            Row(
-                modifier = Modifier.fillMaxSize(),
-                horizontalArrangement = Arrangement.SpaceAround
-            ) {
-                Text(
-                    text = carList.year.toString(),
-                    fontSize = 11.sp,
-                    fontWeight = FontWeight.SemiBold,
-                )
-                Text(
-                    text = carList.make,
-                    fontSize = 11.sp,
-                    fontWeight = FontWeight.SemiBold,
-                )
-                Text(
-                    text = carList.model,
-                    fontSize = 11.sp,
-                    fontWeight = FontWeight.SemiBold,
-                )
-                Text(
-                    text = carList.trim,
-                    fontSize = 11.sp,
-                    fontWeight = FontWeight.SemiBold,
-                )
-            }
-            Row() {
-                Text(
-                    text = carList.currentPrice.toString(),
-                    fontSize = 11.sp,
-                    fontWeight = FontWeight.SemiBold,
-                )
-                Text(
-                    text = carList.mileage.toString(),
-                    fontSize = 11.sp,
-                    fontWeight = FontWeight.SemiBold,
-                )
-
-            }
-            Text(
-                text = carList.dealer.city + " " + carList.dealer.state,
-                fontSize = 11.sp,
-                fontWeight = FontWeight.SemiBold,
-
-                )
-            SimpleButton(text = stringResource(R.string.call_dealer)) {
-
-            }
-        }
-    }
-}
-
-@Composable
-fun SimpleButton(text: String, onClick: () -> Unit) {
-    Button(onClick = onClick) {
-        Text(text = text)
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun SimpleButtonPrev(text: String = "Hello", onClick: () -> Unit = {}) {
-    ConstraintLayout {
-        val context = LocalContext.current
-        val clickAction = {
-            Toast.makeText(context, "Hello", Toast.LENGTH_LONG).show()
-        }
-        Spacer(modifier = Modifier.size(16.dp))
-        val (button, text) = createRefs()
-        Button(
-
-            onClick = clickAction, modifier = Modifier
-                .constrainAs(button) {
+                .constrainAs(lazyColum) {
                     top.linkTo(parent.top, margin = 16.dp)
                     start.linkTo(parent.start, margin = 16.dp)
+                    end.linkTo(parent.end, margin = 16.dp)
+                    bottom.linkTo(parent.bottom, margin = 16.dp)
+                    width = Dimension.fillToConstraints
+                    height = Dimension.fillToConstraints
                 }
-                .wrapContentHeight()
-                .wrapContentWidth()
-        ) { Text(text = "click me !") }
-        Text(
-            text = "Hello world",
-            modifier = Modifier
-                .constrainAs(text) {
-                    top.linkTo(button.bottom)
-                    start.linkTo(button.start)
-                }
-                .wrapContentSize(Alignment.TopStart),
-            style = MaterialTheme.typography.h6,
-            fontWeight = FontWeight.W900,
-            fontFamily = FontFamily.Default
-        )
+                .background(Color.White)
+
+        ) {
+            items(listings.value) { listing ->
+                ProfileContent(listing = listing, modifier = Modifier.wrapContentSize())
+            }
+
+        }
     }
 }
 
+@OptIn(ExperimentalAnimationGraphicsApi::class)
 @Composable
-fun CustomCircularProgressBar() {
-    CircularProgressIndicator(
-        modifier = Modifier.size(width = 100.dp, height = 100.dp),
-        color = MaterialTheme.colors.primary,
-        strokeWidth = 10.dp
+fun LoadingAnimations() {
+
+    val image = AnimatedImageVector.animatedVectorResource(R.drawable.loading_animation)
+    val atEnd by remember { mutableStateOf(false) }
+    Icon(
+        painter = rememberAnimatedVectorPainter(image, atEnd),
+        contentDescription = null,
+        modifier = Modifier.fillMaxSize()
     )
+}
+
+@OptIn(ExperimentalAnimationGraphicsApi::class)
+@Preview
+@Composable
+fun LoadingAnimationsPreview() {
+    Box(modifier = Modifier.wrapContentSize()){
+
+        val image = AnimatedImageVector.animatedVectorResource(R.drawable.loading_animation)
+        var atEnd by remember { mutableStateOf(false) }
+        Image(
+            painter = rememberAnimatedVectorPainter(image, atEnd),
+            contentDescription = stringResource(id = R.string.imageloader),
+            modifier = Modifier.wrapContentSize(align = Alignment.Center)
+
+        )
+        DisposableEffect(Unit) {
+            atEnd = !atEnd
+            onDispose {
+            }
+        }
+    }
 
 }
